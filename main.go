e694880e2d15d651133caa708b0ef0858316066c
Joao <joao.khalaf@gmail.com>
2025-03-26 23:08:59 -0300

    start server

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/jkhf/go_project/internal/app"
 )
 
@@ -9,5 +12,19 @@ func main() {
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
+	}
+
+	app.Logger.Println(("Runing app"))
+
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
 	}
 }
