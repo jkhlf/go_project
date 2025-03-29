efdf45759cae217f993c0abf4464edd4f3f214f0
Joao <joao.khalaf@gmail.com>
2025-03-29 19:48:42 -0300

    add health check endpoint to main application

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -16,6 +17,7 @@ func main() {
 
 	app.Logger.Println(("Runing app"))
 
+	http.HandleFunc("/health", HealthCheck)
 	server := &http.Server{
 		Addr:         ":8080",
 		IdleTimeout:  time.Minute,
@@ -28,3 +30,9 @@ func main() {
 		app.Logger.Fatal(err)
 	}
 }
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Status is avaible\n")
+}
+
+//curl localhost:8080/health
