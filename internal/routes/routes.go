c04522f97c6e3f288341811334cc99ab52ac035e
Joao <joao.khalaf@gmail.com>
2025-04-01 21:38:32 -0300

    add Docker configuration and update routes for workout handling

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -5,10 +5,15 @@ import (
 	"github.com/jkhf/go_project/internal/app"
 )
 
+//curl localhost:8080/workouts/2
+//curl localhost:8080/workouts
+
 func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Get("/health", app.HealthCheck)
+	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
 
+	r.Post("/workouts", app.WorkoutHandler.HandleGetWorkout)
 	return r
 }
